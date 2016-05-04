c888c1aab853dcf02f323252ce156347f9be42d6
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-05-04 17:35:23 +0200

    Pick only color

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ module.exports = function getCssColors(string) {
     throw new TypeError('get-css-colors expected a string')
   }
 
-  var colorListRegex = cssColorList().join('|')
+  string = string.replace(/;|:/, ' ')
+  var colorListRegex = cssColorList().map(function (color) {
+    return ' ' + color + ' '
+  }).join('|')
   var rgbOrRgbaRegex = rgbRegex().source + '|' + rgbaRegex().source
   var hslOrHslaRegex = hslRegex().source + '|' + hslaRegex().source
   var hexRegex = '#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\\b'
--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 var assert = require('assert')
 var getCssColors = require('..')
 
-var stringWithColors = 'foobar hsl(,,) HSLA(1, 1.111%, 1.1111%, .8) rgba(123, 123, 123, .8) #fff turtles rebeccapurple #123AAA'
+var stringWithColors = 'foobar hsl(,,) HSLA(1, 1.111%, 1.1111%, .8) rgba(123, 123, 123, .8) #fff turtles rebeccapurple #123AAA .yellow-button { color: red; }'
 
 describe('get-css-colors', function() {
 
   it('should do something awesome', function() {
     assert.deepEqual(
       getCssColors(stringWithColors),
-      ['HSLA(1, 1.111%, 1.1111%, .8)', 'rgba(123, 123, 123, .8)', '#fff', 'rebeccapurple', '#123AAA']
+      ['HSLA(1, 1.111%, 1.1111%, .8)', 'rgba(123, 123, 123, .8)', '#fff', 'rebeccapurple', '#123AAA', 'red']
     )
   })
 })
